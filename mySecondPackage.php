8554cab39e919e1cc9a44f19e7cf7ddcf01e72fd
bneglect <954826168@qq.com>
2019-12-20 18:46:48 +0800

    第一次失败了，第二次尝试

---
+++ mySecondPackage.php
@@ -0,0 +1,12 @@
+<?php
+
+function dd($param)
+{
+	if(is_array($param)){
+		print_r($param);exit;
+	}elseif(is_bool($param) || is_object($param)){
+		var_dump($param);exit;
+	}else{
+		print_r($param);
+	}
+}
